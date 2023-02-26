cadacf68521b2cb21d0a8488d423c59f04371ed4
shjeon97 <shjeon.dev@gmail.com>
2023-02-26 18:11:22 +0900

    refactoring

--- backend/src/auth/auth.resolver.ts
+++ backend/src/auth/auth.resolver.ts
@@ -6,7 +6,7 @@ import * as bcrypt from 'bcrypt';
 import { UserStatus } from 'src/user/entity/user.entity';
 import { LogService } from 'src/log/log.service';
 import { LogType } from 'src/log/entity/log.entity';
-import { RealIP } from 'nestjs-real-ip';
+import { UserIp } from './decorator/ip.decorator';
 
 @Resolver()
 export class AuthResolver {
@@ -18,48 +18,55 @@ export class AuthResolver {
 
   @Mutation(() => LoginOutput)
   async login(
-    @RealIP() ip: string,
+    @UserIp() ip: string,
     @Args('input') { email, password }: LoginInput,
   ): Promise<LoginOutput> {
-    const user = await this.userService.findOneByEmail(email, true);
+    try {
+      const user = await this.userService.findOneByEmail(email, true);
 
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return {
-        ok: false,
-        error: '존재하지 않는 유저입니다',
-      };
-    }
-
-    switch (user.status) {
-      case UserStatus.Unapproved:
-        return {
-          ok: false,
-          error: '관리자 승인 후 로그인 가능합니다',
-        };
-      case UserStatus.Resignation:
+      if (!user || !(await bcrypt.compare(password, user.password))) {
         return {
           ok: false,
           error: '존재하지 않는 유저입니다',
         };
-    }
+      }
 
-    const token = await this.authService.getAccessToken(user);
+      switch (user.status) {
+        case UserStatus.Unapproved:
+          return {
+            ok: false,
+            error: '관리자 승인 후 로그인 가능합니다',
+          };
+        case UserStatus.Resignation:
+          return {
+            ok: false,
+            error: '존재하지 않는 유저입니다',
+          };
+      }
 
-    if (!token) {
+      const token = await this.authService.getAccessToken(user);
+
+      if (!token) {
+        return {
+          ok: false,
+          error: 'token 생성 실패',
+        };
+      }
+
+      this.logService.create({
+        type: LogType.Login,
+        message: `user id : ${user.id} | ip : ${ip} `,
+      });
+
+      return {
+        ok: true,
+        token: token,
+      };
+    } catch (error) {
       return {
         ok: false,
-        error: 'token 생성 실패',
+        error: '로그인 실패',
       };
     }
-
-    this.logService.create({
-      type: LogType.Login,
-      message: `user id : ${user.id} | ip : ${ip} `,
-    });
-
-    return {
-      ok: true,
-      token: token,
-    };
   }
 }
